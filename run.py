17cb2567e07e89a562554cac687b3a00940b63c7
KMaina <maina.ken0@gmail.com>
2019-02-05 23:32:07 +0300

    Add intial setups

---
+++ run.py
@@ -0,0 +1,11 @@
+import os
+
+from app import create_app
+
+config_name = os.getenv('FLASK_ENV')
+app = create_app(config_name)
+
+port = os.environ.get("PORT", 5000)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
